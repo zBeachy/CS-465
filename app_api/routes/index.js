b947dd6ddc32ebab9d41f88db8b011fadaae8c3d
Zach Beachy <zbeachy@leolabs.space>
2024-12-08 20:05:57 -0600

    Module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express'); // Express app
+const router = express.Router();    // Router logic
+
+// This is where we import the controllers we will route
+const tripsController = require('../controllers/trips');
+
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList); // GET method routes tripList
+
+// GET method routes tripsFindByCode - requires param
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
+    
+module.exports = router;
